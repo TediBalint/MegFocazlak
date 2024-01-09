d41dc43a8709e4e5a56d428a6e00c515f82c4914
TediBalint <tedi.balint@students.jedlik.eu>
2024-01-09 21:29:36 +0100

    CardColors

--- cardData.js
+++ cardData.js
@@ -1,9 +1,8 @@
-let cardData = [
-
-]
+let cardData = []
+const colors = ['bronze', 'bronze-rare', 'silver', 'silver-rare', 'gold', 'gold-rare'];
 for (let x = 0; x < 10; x++) {
    for (let y = 0; y < 5; y++) {
-    cardData.push(`${x};${y};gold-rare;argentina;psg;Messi;rw;12;12;6`)
+    cardData.push(`${x};${y};${colors[parseInt(Math.random()*6)]};argentina;psg;Messi;rw;12;12;6`)
    }
     
 }
