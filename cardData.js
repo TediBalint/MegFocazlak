552e0ba32e67cb607c8d447577055d0a8b8e1b6d
kecske5000 <marton.kristof@students.jedlik.eu>
2024-01-10 09:28:09 +0100

    emberek adatai in progress

--- cardData.js
+++ cardData.js
@@ -1,9 +1,15 @@
 let cardData = [
-   '0;0;bronze;argentina;psg;Banana;st;12;12;6',
-   '1;0;bronze;argentina;psg;Dick;gk;12;12;6',
-   '2;0;silver-rare;argentina;psg;Fuchks;rw;12;12;6',
-   '3;0;gold;argentina;psg;Dickmann;lb;12;12;6',
-   '4;0;bronze-rare;argentina;psg;Nutz;rw;12;12;6'
+   '0;0;bronze;kamerun;bengaluru_banancsapata;Banana;cb;12;12;6',
+   '1;0;bronze;nemetorszag;Kaiserslautern_dickcsapata;Dick;rb;12;12;6',
+   '2;0;silver-rare;ausztria;charlotte_fuckscsapata;Fuchks;lb;12;12;6',
+   '3;0;gold;olaszorszag;brescia_dickmanncsapata;Dickmann;rw;12;12;6',
+   '4;0;silver;ausztria;wolfsberger_nutzcsapata;Nutz;lw;12;12;6',
+   '0;1;silver-rare;argentina;aldosivi_assmanncsapata;Assmann;gk;12;12;6',
+   '1;1;bronze;franciaorszag;psg;Kolo Muani;st;12;12;6',
+   '2;1;gold;argentina;astonvilla;Martinez;gk;12;12;6',
+   '3;1;gold-rare;franciaorszag;psg;Mbappe;lw;12;12;6',
+   '4;1;silver-rare;argentina;miami;Messi;rw;12;12;6',
+   '0;2;bronze-rare;eszakirorszag;aarhus_peacockcsapata;peacock;gk;12;12;6',
 ]
 const colors = ['bronze', 'bronze-rare', 'silver', 'silver-rare', 'gold', 'gold-rare'];
 
